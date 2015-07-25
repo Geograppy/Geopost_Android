28d830ec0874fb9ff5c21f5ae82e62c6f69566b8
Geograppy <geograppy@gmail.com>
2015-07-25 16:56:54 +0200

    Test new branch

--- app/src/main/java/com/geograppy/geopost/MainActivity.java
+++ app/src/main/java/com/geograppy/geopost/MainActivity.java
@@ -88,7 +88,7 @@ public class MainActivity extends ActionBarActivity implements OnUserNameFetched
     private int mNotificationsCount;
 
 
-//this is a test for the new branch
+//this is testtest
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
